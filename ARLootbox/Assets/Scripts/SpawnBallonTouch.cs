24bfe031d0fe9f77e031702a00a33e1d98996e9e
Steve Maier <ysfred4@hotmail.com>
2018-03-06 19:40:49 -0500

    first push with ARKit sample scene

---
+++ ARLootbox/Assets/Scripts/SpawnBallonTouch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+
+public class SpawnBallonTouch : MonoBehaviour
+{
+
+    public GameObject spherePrefab;
+    public float force = 5.0f;
+
+    private Rigidbody rb;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
+        {
+            GameObject go = Instantiate(spherePrefab, transform.position, transform.rotation);
+            go.GetComponent<Rigidbody>().velocity = transform.forward * force;
+        }
+    }
+}
