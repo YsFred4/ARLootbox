cff44ee7171264ca2e89198ed94d0007b47efee6
WarpedTime <silverstorm79@gmail.com>
2018-03-23 18:28:44 -0400

    added test scene
    just added small test scene and script for billbording world-space ui indicators/objects

---
+++ ARLootbox/Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+/*makes an object align/rotate to face towards an object*/
+public class LookAtTarget : MonoBehaviour {
+
+	//object to face
+	[SerializeField] GameObject target;
+
+	// Use this for initialization
+	void Start () { }
+
+	// Update is called once per frame
+	void Update () {
+
+		//orient to face target object
+		if (target != null) {
+			transform.LookAt (target.transform.position);
+		}
+		//otherwise just look at main camera
+		else transform.LookAt(Camera.main.transform);
+	}
+}
