69a91a5ba2d81bc3f5615c2b421eb7ff2710c43a
Steve Maier <ysfred4@hotmail.com>
2018-03-30 17:46:04 -0400

    animation

---
+++ ARLootbox/Assets/Scripts/ClickTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTest : MonoBehaviour
+{
+    public Animator boxAnimation;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (hit.collider.gameObject.tag == "MyBox")
+            {
+                boxAnimation.SetTrigger("openBox");     
+            }
+        }
+    }
+}
